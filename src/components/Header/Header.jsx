6658dfda419c835a90d7d41edc3bca4980bc2097
dkb4rb <jjuannca651@gmail.com>
2022-03-23 07:46:03 +0000

    sent to trash code

--- src/components/Header/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import "./header.css"
-function Header() {
-	return (
-		<header class="AppWelcome">HiSsageS</header>
-	)
-}
-
-export { Header }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from "./App";
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const db = require("./DB/database");
+const usuarios = require("./Routes/usuarios");
+const port = process.env.PORT || 3030;
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+(async () => {
+  try {
+    await db.authenticate();
+    await db.sync();
+    console.log("Conectados a la base de datos");
+  } catch (error) {
+    throw new Error(error);
+  }
+})();
+
+app.use(express.json());
+app.use(cors());
+app.use("/usuarios", usuarios);
+
+app.get("/", (res, req) => {
+  res.json({
+    msg: "BIenvenidos a Hiss4g3s",
+  });
+});
+
+app.listen(port, () => {
+  console.log("El servicio se inicio en el puerto: ", port);
+});
