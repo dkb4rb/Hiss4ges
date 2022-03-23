65707eda7101d7de61220227bfdeffc0cdc2b152
knia10 <kelly.stephania@hotmail.com>
2022-03-23 01:34:44 -0500

    Add register route

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { BrowserRouter, Switch,  Route } from 'react-router-dom';
 import Login from "../components/login/Login";
+import Login from "../components/register/Register";
 import Menu from "../components/Menu/Menu";
 
 function Routes(){
 
     return(
-
-        
         <BrowserRouter>
             <Switch>
 
                 <Route exact path="/" component={Menu}/>
                 <Route exact path="/Login" component={Login}/>
+                <Route exact path="/Register" component={Register}/>
             </Switch>
         </BrowserRouter>
     )
